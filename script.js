35e857a37322bd6ba896830fcf35b4bb9e1a7919
Jiaxin(Peter) Liu <peterliu8368@gmail.com>
2021-07-25 16:09:41 -0700

    completes project

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+let numOfRequestElement = document.querySelector(".num-of-request");
+let count = numOfRequestElement.innerText;
+let connectionCount = document.querySelector(".num-of-connection").innerText;
+
 function changeName() {
     document.querySelector("#profile-name").innerText = "Peter Liu";
 }
+
+function decline(num) {
+    document.querySelector(`.friend-request${num}`).remove();
+    count --;
+    document.querySelector(".num-of-request").innerText = count;
+}
+
+function accept(num) {
+    document.querySelector(`.friend-request${num}`).remove();
+    count --;
+    connectionCount++;
+    document.querySelector(".num-of-request").innerText = count;
+    document.querySelector(".num-of-connection").innerText = connectionCount;
+}
+
+
